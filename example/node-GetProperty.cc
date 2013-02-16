5d29641be71a850d7ac96decd27d9a04d308d6a9
forhappy <haipingf@gmail.com>
2013-02-16 21:45:37 +0800

    add node testcase GetProperty.

---
+++ example/node-GetProperty.cc
@@ -0,0 +1,36 @@
+/* graphdb node testcase class.
+ *
+ * Copyright (c) 2012-2013 Fu Haiping <haipingf@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <iostream>
+#include "Node.h"
+#include "Property.h"
+#include "PropertyValue.h"
+#include "GraphDatabase.h"
+
+using namespace neo4jcpp;
+
+int main(int argc, const char *argv[])
+{
+    GraphDatabase graphdb("http://127.0.0.1:7474");
+    graphdb.Connect();
+    Node node = graphdb.GetNodeByID("2");
+    PropertyValue value = node.GetProperty("hi");
+    std::cout << value.GetString() << std::endl;
+    return 0;
+}
